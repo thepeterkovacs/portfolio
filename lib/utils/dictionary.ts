071ee9cff70de868cdb01491b11668c820519e63
thepeterkovacs <peterkov37@gmail.com>
2023-08-22 16:50:32 +0200

    add dictionary util and lang files

---
+++ lib/utils/dictionary.ts
@@ -0,0 +1,10 @@
+import "server-only"
+
+import type { Locale } from "./i18n"
+
+const dictionaries = {
+	en: () => import("@/lang/en.json").then((module) => module.default),
+	hu: () => import("@/lang/hu.json").then((module) => module.default),
+}
+
+export const getDictionary = async (locale: Locale) => dictionaries[locale]()
