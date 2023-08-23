aec8654c19ff09c860952cd39585d2ed551d018a
thepeterkovacs <peterkov37@gmail.com>
2023-08-23 14:55:19 +0200

    change i18n from const

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,6 @@ function getLocale(request: NextRequest): string | undefined {
 	const negotiatorHeaders: Record<string, string> = {}
 	request.headers.forEach((value, key) => (negotiatorHeaders[key] = value))
 
-	// @ts-ignore locales are readonly
 	const locales: string[] = i18n.locales
 	const languages = new Negotiator({ headers: negotiatorHeaders }).languages()
 
