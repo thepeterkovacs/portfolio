fe4be29ed09ce54459f1d1281f20de28545c8d01
thepeterkovacs <peterkov37@gmail.com>
2023-08-24 10:13:13 +0200

    details to assets

---
+++ lib/assets/details.ts
@@ -0,0 +1,10 @@
+const details = {
+	links: {
+		twitter: "https://twitter.com/thepeterkovacs",
+		gitHub: "https://github.com/thepeterkovacs",
+		linkedIn: "https://www.linkedin.com/in/péter-kovács-418495240",
+	},
+	email: "peterkov37@gmail.com",
+}
+
+export default details
