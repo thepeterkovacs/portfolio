d11eeae49d59837391ba5743b920e4607cf5642e
thepeterkovacs <peterkov37@gmail.com>
2023-08-24 21:14:28 +0200

    add instagram to details

--- lib/assets/details.ts
+++ lib/assets/details.ts
@@ -3,6 +3,7 @@ const details = {
 		twitter: "https://twitter.com/thepeterkovacs",
 		gitHub: "https://github.com/thepeterkovacs",
 		linkedIn: "https://www.linkedin.com/in/péter-kovács-418495240",
+		instagram: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
 	},
 	email: "peterkov37@gmail.com",
 }
