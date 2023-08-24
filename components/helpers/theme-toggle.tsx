eb8b27ed1c582cee9f6c9addc3e8d952d7a83c85
thepeterkovacs <peterkov37@gmail.com>
2023-08-24 21:14:05 +0200

    theme-toggle icons change

--- components/helpers/theme-toggle.tsx
+++ components/helpers/theme-toggle.tsx
@@ -1,10 +1,10 @@
 "use client"
 
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
 
 import * as React from "react"
 
-import Icons from "@/components/helpers/icons"
 import { Button } from "@/components/layout/button"
 
 export default function ThemeToggle() {
@@ -15,8 +15,8 @@ export default function ThemeToggle() {
 			variant='ghost'
 			size='sm'
 			onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-			<Icons.sun className='rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-			<Icons.moon className='absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+			<SunIcon className='w-6 h-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+			<MoonIcon className='w-6 h-6 absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
 			<span className='sr-only'>Toggle theme</span>
 		</Button>
 	)
