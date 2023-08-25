86872f1d5451764fcde6bfc6db505231116c9a3c
thepeterkovacs <peterkov37@gmail.com>
2023-08-25 23:23:54 +0200

    add contacts element

---
+++ app/[locale]/(elements)/contacts.tsx
@@ -0,0 +1,35 @@
+import {
+	EnvelopeClosedIcon,
+	GitHubLogoIcon,
+	InstagramLogoIcon,
+	LinkedInLogoIcon,
+	TwitterLogoIcon,
+} from "@radix-ui/react-icons"
+
+import details from "@/lib/assets/details"
+
+import Contact from "@/components/contact"
+
+const { links, email } = details
+
+export default function Contacts(): JSX.Element {
+	return (
+		<div className='flex w-72 justify-between'>
+			<Contact href={links.twitter} target='_blank'>
+				<TwitterLogoIcon className='w-8 h-8' />
+			</Contact>
+			<Contact href={links.gitHub} target='_blank'>
+				<GitHubLogoIcon className='w-7 h-7' />
+			</Contact>
+			<Contact href={`mailto:${email}`}>
+				<EnvelopeClosedIcon className='w-7 h-7' />
+			</Contact>
+			<Contact href={links.linkedIn} target='_blank'>
+				<LinkedInLogoIcon className='w-7 h-7' />
+			</Contact>
+			<Contact href={links.instagram} target='_blank'>
+				<InstagramLogoIcon className='w-8 h-8' />
+			</Contact>
+		</div>
+	)
+}
