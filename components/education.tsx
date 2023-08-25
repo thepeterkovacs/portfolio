a3072dae90b25ce5e022aa6b863b20709ce9a900
thepeterkovacs <peterkov37@gmail.com>
2023-08-25 23:21:10 +0200

    small changes in appearance

--- components/education.tsx
+++ components/education.tsx
@@ -7,7 +7,7 @@ interface Props extends PropsWithChildren {
 export default function Education({ dates, children }: Props): JSX.Element {
 	return (
 		<div className='flex flex-col gap-2'>
-			<div className='text-2xl'>{dates}</div>
+			<div className='text-xl'>{dates}</div>
 			{children}
 		</div>
 	)
