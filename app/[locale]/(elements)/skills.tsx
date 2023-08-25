e9a4e62745996f5382fcb926540195c7e70bd00a
thepeterkovacs <peterkov37@gmail.com>
2023-08-25 23:42:22 +0200

    eslint warning fixes

--- app/[locale]/(elements)/skills.tsx
+++ app/[locale]/(elements)/skills.tsx
@@ -11,12 +11,16 @@ export default function Skills(): JSX.Element {
 			<div className='flex gap-4 text-base flex-wrap lg:flex-nowrap'>
 				<div className='flex flex-col gap-2 w-full lg:w-1/2'>
 					{languages.map((language) => (
-						<Skill name={language.name} value={language.value} />
+						<Skill key={language.name} name={language.name} value={language.value} />
 					))}
 				</div>
 				<div className='flex flex-col gap-2 w-full lg:w-1/2'>
 					{technologies.map((technology) => (
-						<Skill name={technology.name} value={technology.value} />
+						<Skill
+							key={technology.name}
+							name={technology.name}
+							value={technology.value}
+						/>
 					))}
 				</div>
 			</div>
