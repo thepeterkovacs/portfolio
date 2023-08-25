4b9760b00da516b79d2b7a27f057fb0003871b4c
thepeterkovacs <peterkov37@gmail.com>
2023-08-25 17:05:57 +0200

    add experience component

---
+++ components/experience.tsx
@@ -0,0 +1,14 @@
+import { PropsWithChildren } from "react"
+
+interface Props extends PropsWithChildren {
+	dates: string
+}
+
+export default function Experience({ dates, children }: Props): JSX.Element {
+	return (
+		<div className='flex flex-col gap-2'>
+			<div className='text-2xl'>{dates}</div>
+			{children}
+		</div>
+	)
+}
